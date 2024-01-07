5da3543086c592c2fb81797d71f3f87acda5577e
mohammedterfa <mohammedterfa@gmail.com>
2024-01-07 19:21:00 +0300

    add order header and details repository

---
+++ Bulky/Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class OrderHeaderyRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+
+        public OrderHeaderyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
