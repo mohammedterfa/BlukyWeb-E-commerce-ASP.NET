77319c07f2c3f14032c5c2db783b3d3b9a4110ec
mohammedterfa <mohammedterfa@gmail.com>
2024-01-09 20:20:02 +0300

    modify OrderHeader Repository

--- Bulky/Bulky.Models/obj/Debug/net7.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky/Bulky.Models/obj/Debug/net7.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d48029b76c5ca5a4f2e721b4479429efd837246")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
